bba97837a00f1e553f10518596d603ad8bf15f02
Giuseppe <venturagiuseppe08@gmail.com>
2023-06-06 10:18:54 +0200

    I added hotels names in Main

--- src/Hotel.java
+++ src/Hotel.java
@@ -4,11 +4,13 @@ public class Hotel
     private double price;
 
     private String city;
+    private int ID_booking;
 
-    public Hotel(String name, double price, String city) {
+    public Hotel(String name, double price, String city, int ID_booking) {
         this.name = name;
         this.price = price;
         this.city = city;
+        this.ID_booking= ID_booking;
     }
 
     public String getName() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Hotel> list_hotel= new ArrayList<Hotel>();
+
+
+        list_hotel.add( new Hotel("Hotel Catalunya",60.99,"alghero",1));
+        list_hotel.add(new Hotel("Hotel Carolos V",70.99,"alghero",02));
+        list_hotel.add(new Hotel("Hotel Turin",69.99,"aosta",2));
+        list_hotel.add(new Hotel("Grande Albergo delle Nazioni",50.99,"bari",3));
+        list_hotel.add( new Hotel("Hotel Metropolitan",50.10,"bologna",4));
+        list_hotel.add( new Hotel("Hotel Dall'Ara",79.99,"bologna",5));
+        list_hotel.add( new Hotel("Hotel Sardegna",49.99,"cagliari",6));
+        list_hotel.add( new Hotel("Hotel Regina Margherita",99.99,"cagliari",7));
+        list_hotel.add( new Hotel("Hotel Nettuno",45.99,"catania",8));
+        list_hotel.add( new Hotel("Hotel Brunelleschi",100.99,"firenze",9));
+        list_hotel.add( new Hotel("Hotel Continentale",89.99,"firenze",10));
+        list_hotel.add( new Hotel("Hotel Dante",79.99,"firenze",11));
+        list_hotel.add( new Hotel("Hotel Bristol Palace",60.00,"genova",12));
+        list_hotel.add(new Hotel("Hotel Risorgimento Palace",50.00,"lecce",13));
+        list_hotel.add( new Hotel("Hotel Brera",100.00,"milano",14));
+        list_hotel.add(new Hotel("Hotel Milano Scala",120.99,"milano",15));
+        list_hotel.add(new Hotel( "Excelsior Hotel Gallia",130.99,"milano",16));
+        list_hotel.add( new Hotel("Hotel De Laurentis",80.99,"napoli",17));
+        list_hotel.add( new Hotel("Grand Hotel Vesuvio",79.99,"napoli",18));
+        list_hotel.add( new Hotel("Hotel Romeo",99.99,"napoli",19));
+        list_hotel.add( new Hotel("Hotel Brufani Palace",100.00,"perugia,",20));
+        list_hotel.add( new Hotel("Hotel de Londres",48.99,"rimini",21));
+        list_hotel.add(new Hotel("Grand Hotel Rimini",71.99,"rimini",22));
+        list_hotel.add( new Hotel("Hotel Colosseo",119.99,"roma",23));
+        list_hotel.add( new Hotel("Hotel Hassler",150.99,"roma",24));
+        list_hotel.add(new Hotel("Hotel Eden",140.00,"roma",25));
+        list_hotel.add(new Hotel("Hotel Parioli",170.99,"roma",26));
+        list_hotel.add( new Hotel("Hotel Agnelli",78.95,"torino",27));
+        list_hotel.add( new Hotel("Hotel Turin Palace",98.95,"torino",28));
+        list_hotel.add( new Hotel("Grand Hotel Trento",50.00,"trento",29));
+        list_hotel.add( new Hotel("Hotel Danieli",190.99,"venezia",30));
+        list_hotel.add( new Hotel("Hotel Palazzo Barbarigo",210.99,"venezia",31));
+        list_hotel.add( new Hotel("Hotel Venezia",180.00,"venezia",32));
+        list_hotel.add( new Hotel("Hotel Romeo-Giulietta",140.00,"verona",33));
+        list_hotel.add( new Hotel("Hotel Due Torri",168.99,"verona",35));
+
+
     }
 
 
